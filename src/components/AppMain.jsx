863a27cb03450941a995c39f09d9f2951ae24058
andrealoda <andrea@casaloda.it>
2026-04-16 21:07:22 +0200

    jumbotron wip

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,10 +1,30 @@
+import jumbotronImg from '../assets/img/jumbotron.jpg'
+import comics from '../data/comics.js'
+
 export default function AppMain() {
 
     return (
 
         <main className="bg-black">
+            <div className="jumbotronHead ">
+                <img src={jumbotronImg} alt="" />
+
+            </div>
             <div className="container text-white text-start">
-                <h1>-- Content goes here --</h1>
+                <div className="jumbotron d-flex flex-wrap justify-content-center gap-5 mt-5">
+
+                    {comics.map((item) => (
+                        <div className="card comic-card m-1" key={item.id}>
+                            <img src={item.thumb} alt="" className='thumb' />
+                        </div>
+                    ))}
+
+
+                </div>
+                <div className="d-flex justify-content-center py-4 mt-2">
+                    <button className="btn btn-primary px-5">LOAD MORE</button>
+                </div>
+
             </div>
         </main>
 
