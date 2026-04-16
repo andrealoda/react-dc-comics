8aa7674c828ef8d4bf5ddd30290c453d20e280a0
andrealoda <andrea@casaloda.it>
2026-04-16 09:45:25 +0200

    implement utility bar

--- src/components/UtilityBar.jsx
+++ src/components/UtilityBar.jsx
@@ -1,8 +1,39 @@
+import comics from '../assets/img/buy-comics-digital-comics.png'
+import merchandise from '../assets/img/buy-comics-merchandise.png'
+import subscription from '../assets/img/buy-comics-subscriptions.png'
+import locator from '../assets/img/buy-comics-shop-locator.png'
+import visa from '../assets/img/buy-dc-power-visa.svg'
+
+
 export default function UtilityBar() {
 
     return (
 
-        <div>Utility Bar</div>
+        <section className="utilityBar">
+            <div className="container">
+                <div className="link">
+                    <img src={comics} alt="" />
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div className="link">
+                    <img src={merchandise} alt="" />
+                    <span>DC MERCHANDISE</span>
+                </div>
+                <div className="link">
+                    <img src={subscription} alt="" />
+                    <span>SUBSCRIPTION</span>
+                </div>
+                <div className="link">
+                    <img src={locator} alt="" />
+                    <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div className="link">
+                    <img src={visa} alt="" />
+                    <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </section>
+
 
     )
 }
