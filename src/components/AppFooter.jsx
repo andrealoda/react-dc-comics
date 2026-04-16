bc048badb71c64964a9a50257af3759468b743f5
andrealoda <andrea@casaloda.it>
2026-04-16 10:45:50 +0200

    implement footer -wip

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,8 +1,28 @@
+import logoBg from '../assets/img/dc-logo-bg.png'
+
 export default function AppFooter() {
 
     return (
 
-        <footer>Footer</footer>
+        <footer>
+            <div className="container d-flex justify-content-between">
+                <div className="footerLink">
+                    <ul>
+                        <li className='title'>DC COMICS</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div className="footerLogo">
+                    <img src={logoBg} alt="" />
+                </div>
+            </div>
+        </footer >
 
     )
 }
