76d9511af2f619c2324247ccd22c1598d89bc04f
andrealoda <andrea@casaloda.it>
2026-04-17 10:37:30 +0200

    implement jumbotron

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -11,18 +11,20 @@ export default function AppMain() {
 
             </div>
             <div className="container text-white text-start">
+                <button className="btn btn-primary rounded-0 px-5 negative-translation-50">CURRENT SERIES</button>
                 <div className="jumbotron d-flex flex-wrap justify-content-center gap-5 mt-5">
 
                     {comics.map((item) => (
-                        <div className="card comic-card m-1" key={item.id}>
+                        <div className="card comic-card m-1 bg-black" key={item.id}>
                             <img src={item.thumb} alt="" className='thumb' />
+                            <span className="comics-text text-white mt-2">{item.title}</span>
                         </div>
                     ))}
 
 
                 </div>
                 <div className="d-flex justify-content-center py-4 mt-2">
-                    <button className="btn btn-primary px-5">LOAD MORE</button>
+                    <button className="btn btn-primary rounded-0 px-5">LOAD MORE</button>
                 </div>
 
             </div>
