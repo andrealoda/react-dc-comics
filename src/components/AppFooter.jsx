15fdc34410b015bf0b977dcec76e07048b0e0328
andrealoda <andrea@casaloda.it>
2026-04-18 15:08:07 +0200

    refactor AppFooter to update section titles and enhance footer content organization

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -56,35 +56,43 @@ export default function AppFooter() {
                         </ul>
                     </div>
                     <div className="col">
-                        <h5 className='title'>DC COMICS</h5>
+                        <h3 className='title'>SHOP</h3>
                         <ul>
-                            {dcComics.map((item) => (
-                                <li key={item.id} >
-                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
-                                </li>
-                            ))}
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
                         </ul>
                     </div>
                     <div className="col">
-                        <h5 className='title'>DC COMICS</h5>
+                        <h3 className='title'>DC</h3>
                         <ul>
-                            {dcComics.map((item) => (
-                                <li key={item.id} >
-                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
-                                </li>
-                            ))}
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choises</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent workshop</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
                         </ul>
                     </div>
                     <div className="col">
-                        <h5 className='title'>DC COMICS</h5>
+                        <h3 className='title'>SITES</h3>
                         <ul>
-                            {dcComics.map((item) => (
-                                <li key={item.id} >
-                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
-                                </li>
-                            ))}
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
                         </ul>
                     </div>
+
+                    {/* costruire con row row-cols-1 row-cols-sm-2... + cols + liste bootstrap */}
+
+
+
                 </div>
                 <div className="footerLogo">
                     <img src={logoBg} alt="" />
