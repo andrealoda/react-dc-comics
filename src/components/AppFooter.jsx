25ef4ad1bf835d0b3b9eab7344512ba1e053cfc1
andrealoda <andrea@casaloda.it>
2026-04-18 15:07:22 +0200

    refactor AppFooter and UtilityBar to enhance code organization and improve responsiveness

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,52 +1,90 @@
 import logoBg from '../assets/img/dc-logo-bg.png'
 
+const dcComics = [
+    {
+        id: 1,
+        text: "Characters",
+        link: "/characters"
+    },
+    {
+        id: 2,
+        text: "Comics",
+        link: "/comics"
+    },
+    {
+        id: 3,
+        text: "Movies",
+        link: "/movies"
+    },
+    {
+        id: 4,
+        text: "TV",
+        link: "/tv"
+    },
+    {
+        id: 5,
+        text: "Games",
+        link: "/games"
+    },
+    {
+        id: 6,
+        text: "Videos",
+        link: "/videos"
+    },
+    {
+        id: 7,
+        text: "News",
+        link: "/news"
+    }
+]
+
 export default function AppFooter() {
 
     return (
 
         <footer>
             <div className="container d-flex justify-content-between">
-                <div className="footerLink">
-                    <ul>
-                        <li className='title'>DC COMICS</li>
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Movies</li>
-                        <li>TV</li>
-                        <li>Games</li>
-                        <li>Videos</li>
-                        <li>News</li>
-                    </ul>
-                    <ul>
-                        <li className='title'>SHOP</li>
-                        <li>Shop DC</li>
-                        <li>Shop DC Collectibles</li>
-                    </ul>
-                    <ul>
-                        <li className='title'>DC</li>
-                        <li>Terms Of Use</li>
-                        <li>Privacy policy (New)</li>
-                        <li>Ad Choises</li>
-                        <li>Advertising</li>
-                        <li>Jobs</li>
-                        <li>Subscriptions</li>
-                        <li>Talent workshop</li>
-                        <li>CPSC Certificates</li>
-                        <li>Ratings</li>
-                        <li>Shop Help</li>
-                        <li>Contact Us</li>
-                    </ul>
-                    <ul>
-                        <li className='title'>SITES</li>
-                        <li>DC</li>
-                        <li>MAD Magazine</li>
-                        <li>DC Kids</li>
-                        <li>DC Universe</li>
-                        <li>DC Power Visa</li>
-                    </ul>
-
-                    {/* costruire con row row-cols-1 row-cols-sm-2... + cols + liste bootstrap */}
-
+                <div className="footerLink row row-cols-1 row-cols-md-2 row-cols-lg-4">
+                    <div className="col">
+                        <h5 className='title'>DC COMICS</h5>
+                        <ul>
+                            {dcComics.map((item) => (
+                                <li key={item.id} >
+                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5 className='title'>DC COMICS</h5>
+                        <ul>
+                            {dcComics.map((item) => (
+                                <li key={item.id} >
+                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5 className='title'>DC COMICS</h5>
+                        <ul>
+                            {dcComics.map((item) => (
+                                <li key={item.id} >
+                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5 className='title'>DC COMICS</h5>
+                        <ul>
+                            {dcComics.map((item) => (
+                                <li key={item.id} >
+                                    <a className='text-decoration-none text-secondary' href={item.link}>{item.text}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
                 <div className="footerLogo">
                     <img src={logoBg} alt="" />
--- src/components/UtilityBar.jsx
+++ src/components/UtilityBar.jsx
@@ -5,32 +5,51 @@ import locator from '../assets/img/buy-comics-shop-locator.png'
 import visa from '../assets/img/buy-dc-power-visa.svg'
 
 
+const utilityLinks = [
+    {
+        id: 1,
+        text: "DIGITAL COMICS",
+        icon: comics
+    },
+    {
+        id: 2,
+        text: "DC MERCHANDISE",
+        icon: merchandise
+    },
+    {
+        id: 3,
+        text: "SUBSCRIPTION",
+        icon: subscription
+    },
+    {
+        id: 4,
+        text: "COMIC SHOP LOCATOR",
+        icon: locator
+    },
+    {
+        id: 5,
+        text: "DC POWER VISA",
+        icon: visa
+    }
+
+]
+
 export default function UtilityBar() {
 
     return (
 
         <section className="utilityBar">
             <div className="container">
-                <div className="link">
-                    <img src={comics} alt="" />
-                    <span>DIGITAL COMICS</span>
-                </div>
-                <div className="link">
-                    <img src={merchandise} alt="" />
-                    <span>DC MERCHANDISE</span>
-                </div>
-                <div className="link">
-                    <img src={subscription} alt="" />
-                    <span>SUBSCRIPTION</span>
-                </div>
-                <div className="link">
-                    <img src={locator} alt="" />
-                    <span>COMIC SHOP LOCATOR</span>
-                </div>
-                <div className="link">
-                    <img src={visa} alt="" />
-                    <span>DC POWER VISA</span>
-                </div>
+
+
+                {utilityLinks.map((item) => (
+                    <div className="link" key={item.id}>
+                        <a href="#" className='text-decoration-none text-white'>
+                            <img src={item.icon} alt="" />
+                            <span>{item.text}</span>
+                        </a>
+                    </div>
+                ))}
             </div>
         </section>
 
